47cb888493cffe42af95350cb57f22365c5a601c
Sara Damiano <sdamiano@stroudcenter.org>
2025-03-10 16:27:39 -0400

    Increase max autobaud rate
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/TinyGsmCommon.h
+++ src/TinyGsmCommon.h
@@ -116,9 +116,9 @@ const T& TinyGsmMax(const T& a, const T& b) {
  */
 template <class T>
 uint32_t TinyGsmAutoBaud(T& SerialAT, uint32_t minimum = 9600,
-                         uint32_t maximum = 115200) {
-  static uint32_t rates[] = {921600, 115200, 57600, 38400, 19200, 9600,  460800,
-                             230400, 74400,  74880, 2400,  4800,  14400, 28800};
+                         uint32_t maximum = 921600) {
+  static uint32_t rates[] = {115200, 57600, 38400, 19200, 9600, 921600, 460800,
+                             230400, 74400, 74880, 2400,  4800, 14400,  28800};
 
   for (uint8_t i = 0; i < sizeof(rates) / sizeof(rates[0]); i++) {
     uint32_t rate = rates[i];
