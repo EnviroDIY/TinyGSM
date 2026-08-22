257c567f9b76a730dbdb73419f89d5a7f689ff20
Sara Damiano <sdamiano@stroudcenter.org>
2026-08-22 12:13:55 -0400

    Test against SIM_READY
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- examples/AllFunctions/AllFunctions.ino
+++ examples/AllFunctions/AllFunctions.ino
@@ -261,7 +261,7 @@ void loop() {
 
 #if TINY_GSM_TEST_GPRS
   // Unlock your SIM card with a PIN if needed
-  if (GSM_PIN && static_cast<int>(modem.getSimStatus()) != 3) {
+  if (GSM_PIN && modem.getSimStatus() != SIM_READY) {
     modem.simUnlock(GSM_PIN);
   }
 #endif
--- examples/MqttClient/MqttClient.ino
+++ examples/MqttClient/MqttClient.ino
@@ -212,7 +212,7 @@ void setup() {
 
 #if TINY_GSM_USE_GPRS
   // Unlock your SIM card with a PIN if needed
-  if (GSM_PIN && static_cast<int>(modem.getSimStatus()) != 3) {
+  if (GSM_PIN && modem.getSimStatus() != SIM_READY) {
     modem.simUnlock(GSM_PIN);
   }
 #endif
