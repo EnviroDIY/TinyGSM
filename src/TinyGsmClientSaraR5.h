d85f6b284780e0d39a0504fec250b340fa8ac653
Sara Damiano <sdamiano@stroudcenter.org>
2026-08-28 11:39:57 -0400

    Fix moving mux in dynamic muxes
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/TinyGsmClientSaraR5.h
+++ src/TinyGsmClientSaraR5.h
@@ -343,7 +343,7 @@ class TinyGsmSaraR5
         // move any existing client at the assigned mux number to the next
         // available slot
         // set the requested mux to -1 to get the next available mux number
-        at->moveSocket(mux, static_cast<uint8_t>(-1));
+        at->moveSocket(assignedMux, static_cast<uint8_t>(-1));
         // set the client's internal mux number and insert it into the array
         at->sockets[assignedMux] = this;
         mux                      = assignedMux;
