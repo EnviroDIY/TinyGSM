85e830b8894f8f9789600ecd3a5cc9ab7de3c1bb
Sara Damiano <sdamiano@stroudcenter.org>
2026-08-31 18:02:27 -0400

    remove dup calls to findFirstUnassignedMux
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/TinyGsmClientUBLOX.h
+++ src/TinyGsmClientUBLOX.h
@@ -280,12 +280,15 @@ class TinyGsmUBLOX : public TinyGsmModem<TinyGsmUBLOX, TinyGsmUBLOXModemConfig>,
         this->mux = mux;
         // If the mux number is in use or out of range, find the next available
         // one
-      } else if (at->findFirstUnassignedMux() != static_cast<uint8_t>(-1)) {
-        this->mux = at->findFirstUnassignedMux();
       } else {
-        // If we can't find anything available, overwrite something, using mod
-        // to make sure we're in range
-        this->mux = (mux % TcpConfig::kMuxCount);
+        uint8_t nextMux = at->findFirstUnassignedMux();
+        if (nextMux != static_cast<uint8_t>(-1)) {
+          this->mux = nextMux;
+        } else {
+          // If we can't find anything available, overwrite something, using mod
+          // to make sure we're in range
+          this->mux = (mux % TcpConfig::kMuxCount);
+        }
       }
       at->sockets[this->mux] = this;
 
