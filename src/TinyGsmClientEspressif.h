d4e67ad6f2795cc00fa74f848b04b1e73327104f
Sara Damiano <sdamiano@stroudcenter.org>
2026-08-31 12:54:28 -0400

    remove forwarder
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/TinyGsmClientEspressif.h
+++ src/TinyGsmClientEspressif.h
@@ -377,9 +377,11 @@ class TinyGsmEspressif
    * Utilities
    */
  protected:
+#if 0
   bool handleURCs(String& data) {
     return thisModem().handleURCs(data);
   }
+#endif
 
  public:
   /// Stream used to communicate with the modem.
