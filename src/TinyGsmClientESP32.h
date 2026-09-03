72a1d5c17b90bd74a79b36a9587d7325f6e418dd
Sara Damiano <sdamiano@stroudcenter.org>
2026-09-03 14:36:50 -0400

    break before printing
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/TinyGsmClientESP32.h
+++ src/TinyGsmClientESP32.h
@@ -969,13 +969,13 @@ class TinyGsmESP32
         c = stream.read();
         if (c >= 0) break;
       } while (millis() - _startMillis < 50);
-      // Print the file to the buffer
+      if (c < 0) { break; }  // if we run out of characters, stop
 #ifndef DUMP_AT_COMMANDS
+      // Print the file to the buffer
       // NOTE: Only do this if we're not dumping the all AT, or we'll double
       // print
       print_stream.write(c);
 #endif
-      if (c < 0) { break; }  // if we run out of characters, stop
     }
     print_stream.flush();
     // Final OK
